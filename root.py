b8989e6e3d3d04f4dfef68bf9069ffaf33d62619
nu20 <bibhutipatra123@gmail.com>
2025-01-30 18:43:20 +0530

    first commit

---
+++ root.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image , ImageTK
+
+root = Tk()
+root.title('money counter')
+root.configure(bg = 'light blue')
+root.geometry('650x4000')
